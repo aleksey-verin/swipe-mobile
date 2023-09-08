21748a3ff4050e6de115aeaf0b9a06d91c8be206
Aleksey Verin <verevaa@gmail.com>
2023-09-08 09:57:13 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,62 +1,49 @@
-const swipeBlock = document.querySelector<HTMLElement>('.swipe')
-const swipeZone = document.querySelector<HTMLElement>('.swipe-zone')
+const swipeBlock = document.querySelector('.swipe')
+const swipeZone = document.querySelector('.swipe-zone')
 
-swipeZone?.addEventListener('touchstart', touchstartFn)
-swipeZone?.addEventListener('touchmove', touchmoveFn)
-swipeZone?.addEventListener('touchend', touchEndFn)
+swipeZone.addEventListener('touchstart', touchstartFn)
+swipeZone.addEventListener('touchmove', touchmoveFn)
+swipeZone.addEventListener('touchend', touchEndFn)
 
 // 130 от края
-const initialTopPosition = swipeBlock
-  ? parseInt(window.getComputedStyle(swipeBlock, null).getPropertyValue('top'))
-  : 0
-
+const initialTopPosition = parseInt(window.getComputedStyle(swipeBlock,null).getPropertyValue("top"));
 // console.log('initialTopPosition: ', initialTopPosition);
-let yStartPosition = 0
+let yStartPosition = null
 
-function touchstartFn(e: TouchEvent) {
-  e.preventDefault()
-  swipeBlock?.classList.remove('free')
+function touchstartFn (e) {
+  e.preventDefault();
+  swipeBlock.classList.remove('free')
 
-  yStartPosition = +e.touches[0].clientY.toFixed(0) // где коснулись (150)
+  yStartPosition = e.touches[0].clientY.toFixed(0) // где коснулись (150)
   // console.log('yStart: ', yStart);
-  if (swipeZone) {
-    swipeZone.style.backgroundColor = '#607c98'
-  }
+  swipeZone.style.backgroundColor = '#607c98'
 }
 
-function touchmoveFn(e: TouchEvent) {
-  e.preventDefault()
-  const yCurrentPosition = +e.touches[0].clientY.toFixed(0) // вниз 200, наверх 100
+function touchmoveFn (e) {
+  e.preventDefault();
+  const yCurrentPosition = e.touches[0].clientY.toFixed(0) // вниз 200, наверх 100
   // console.log('yCurrent: ', yCurrent);
-  const difference = yCurrentPosition - yStartPosition
+  const difference =  yCurrentPosition - yStartPosition
   // console.log('diff: ', diff);
-  if (swipeBlock && initialTopPosition) {
-    swipeBlock.style.top = `${initialTopPosition + difference}px`
-  }
+  swipeBlock.style.top = `${initialTopPosition + difference}px`
   // console.log(yCurrent);
 }
 
-function touchEndFn(e: TouchEvent) {
-  e.preventDefault()
-  console.log(document.documentElement.clientHeight)
-  swipeBlock?.classList.add('free')
-  const finalTopPosition = swipeBlock
-  ? parseInt(window.getComputedStyle(swipeBlock, null).getPropertyValue('top'))
-  : 0
+function touchEndFn(e) {
+  e.preventDefault();
+  console.log(document.documentElement.clientHeight);
+  swipeBlock.classList.add('free')
+  const finalTopPosition = parseInt(window.getComputedStyle(swipeBlock,null).getPropertyValue("top"))
   // console.log('finalBottomPosition: ', finalTopPosition);
   if (finalTopPosition < initialTopPosition + 50) {
-    if (swipeBlock && swipeZone) {
-      swipeBlock.style.top = `${initialTopPosition}px`
-      swipeZone.style.backgroundColor = '#778899'
-    }
+    swipeBlock.style.top = `${initialTopPosition}px`
+    swipeZone.style.backgroundColor = 'lightslategray'
   } else {
     const screenHeight = document.documentElement.clientHeight
-    if (swipeBlock) {
-      swipeBlock.style.top = `${screenHeight + 200}px`
-      setTimeout(() => {
-        swipeBlock.remove()
-      }, 500)
-    }
+    swipeBlock.style.top = `${screenHeight + 200}px`
+    setTimeout(() => {
+      swipeBlock.remove()
+    }, 500);
   }
 }
 
